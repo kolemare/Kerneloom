23ab5dbc0fab86e7dad325f17b16cb07bae0df17
Marko Kostic <kolemareee@gmail.com>
2026-07-20 02:55:14 +0200

    Expand CUDA MaxPool2d Benchmark Suite

--- tests/common/test_options.hpp
+++ tests/common/test_options.hpp
@@ -57,45 +57,185 @@ namespace kl::test::options
     namespace maxpool2d_forward_float32
     {
 
-        constexpr std::size_t batch_size =
-            448;
+        namespace regular_large
+        {
 
-        constexpr std::size_t channels =
-            64;
+            constexpr std::size_t batch_size =
+                448;
 
-        constexpr std::size_t input_height =
-            256;
+            constexpr std::size_t channels =
+                64;
 
-        constexpr std::size_t input_width =
-            256;
+            constexpr std::size_t input_height =
+                256;
 
-        constexpr std::size_t kernel_height =
-            2;
+            constexpr std::size_t input_width =
+                256;
 
-        constexpr std::size_t kernel_width =
-            2;
+            constexpr std::size_t kernel_height =
+                2;
 
-        constexpr std::size_t stride_height =
-            2;
+            constexpr std::size_t kernel_width =
+                2;
 
-        constexpr std::size_t stride_width =
-            2;
+            constexpr std::size_t stride_height =
+                2;
 
-        constexpr std::size_t padding_height =
-            0;
+            constexpr std::size_t stride_width =
+                2;
 
-        constexpr std::size_t padding_width =
-            0;
+            constexpr std::size_t padding_height =
+                0;
 
-        constexpr std::size_t output_height =
-            (input_height + 2 * padding_height - kernel_height) /
-                stride_height +
-            1;
+            constexpr std::size_t padding_width =
+                0;
 
-        constexpr std::size_t output_width =
-            (input_width + 2 * padding_width - kernel_width) /
-                stride_width +
-            1;
+            constexpr std::size_t output_height =
+                (input_height + 2 * padding_height - kernel_height) /
+                    stride_height +
+                1;
+
+            constexpr std::size_t output_width =
+                (input_width + 2 * padding_width - kernel_width) /
+                    stride_width +
+                1;
+
+        }
+
+        namespace demanding_odd_shape
+        {
+
+            constexpr std::size_t batch_size =
+                257;
+
+            constexpr std::size_t channels =
+                65;
+
+            constexpr std::size_t input_height =
+                255;
+
+            constexpr std::size_t input_width =
+                257;
+
+            constexpr std::size_t kernel_height =
+                2;
+
+            constexpr std::size_t kernel_width =
+                2;
+
+            constexpr std::size_t stride_height =
+                2;
+
+            constexpr std::size_t stride_width =
+                2;
+
+            constexpr std::size_t padding_height =
+                0;
+
+            constexpr std::size_t padding_width =
+                0;
+
+            constexpr std::size_t output_height =
+                (input_height + 2 * padding_height - kernel_height) /
+                    stride_height +
+                1;
+
+            constexpr std::size_t output_width =
+                (input_width + 2 * padding_width - kernel_width) /
+                    stride_width +
+                1;
+
+        }
+
+        namespace padded_stride_one
+        {
+
+            constexpr std::size_t batch_size =
+                128;
+
+            constexpr std::size_t channels =
+                64;
+
+            constexpr std::size_t input_height =
+                224;
+
+            constexpr std::size_t input_width =
+                224;
+
+            constexpr std::size_t kernel_height =
+                3;
+
+            constexpr std::size_t kernel_width =
+                3;
+
+            constexpr std::size_t stride_height =
+                1;
+
+            constexpr std::size_t stride_width =
+                1;
+
+            constexpr std::size_t padding_height =
+                1;
+
+            constexpr std::size_t padding_width =
+                1;
+
+            constexpr std::size_t output_height =
+                (input_height + 2 * padding_height - kernel_height) /
+                    stride_height +
+                1;
+
+            constexpr std::size_t output_width =
+                (input_width + 2 * padding_width - kernel_width) /
+                    stride_width +
+                1;
+
+        }
+
+        namespace padded_stride_two
+        {
+
+            constexpr std::size_t batch_size =
+                256;
+
+            constexpr std::size_t channels =
+                64;
+
+            constexpr std::size_t input_height =
+                255;
+
+            constexpr std::size_t input_width =
+                255;
+
+            constexpr std::size_t kernel_height =
+                3;
+
+            constexpr std::size_t kernel_width =
+                3;
+
+            constexpr std::size_t stride_height =
+                2;
+
+            constexpr std::size_t stride_width =
+                2;
+
+            constexpr std::size_t padding_height =
+                1;
+
+            constexpr std::size_t padding_width =
+                1;
+
+            constexpr std::size_t output_height =
+                (input_height + 2 * padding_height - kernel_height) /
+                    stride_height +
+                1;
+
+            constexpr std::size_t output_width =
+                (input_width + 2 * padding_width - kernel_width) /
+                    stride_width +
+                1;
+
+        }
 
         constexpr double absolute_tolerance =
             1.0e-5;
@@ -113,6 +253,7 @@ namespace kl::test::options
             false;
 
     }
+
 }
 
 #endif // KL_TEST_OPTIONS_HPP
--- tests/suites/cuda_maxpool2d_forward_float32_test.cpp
+++ tests/suites/cuda_maxpool2d_forward_float32_test.cpp
@@ -19,181 +19,366 @@
 
 #include <kernels/cuda/pooling/maxpool2d_cuda_float32.cuh>
 
+#include <cstddef>
+
 namespace
 {
 
     namespace options =
         kl::test::options::maxpool2d_forward_float32;
 
-    kl::Pooling2dOptions make_pooling_options()
+    kl::Pooling2dOptions make_pooling_options(
+        std::size_t kernel_height,
+        std::size_t kernel_width,
+        std::size_t stride_height,
+        std::size_t stride_width,
+        std::size_t padding_height,
+        std::size_t padding_width)
     {
         kl::Pooling2dOptions pooling_options;
 
         pooling_options.kernel_h =
-            options::kernel_height;
+            kernel_height;
 
         pooling_options.kernel_w =
-            options::kernel_width;
+            kernel_width;
 
         pooling_options.stride_h =
-            options::stride_height;
+            stride_height;
 
         pooling_options.stride_w =
-            options::stride_width;
+            stride_width;
 
         pooling_options.padding_h =
-            options::padding_height;
+            padding_height;
 
         pooling_options.padding_w =
-            options::padding_width;
+            padding_width;
 
         return pooling_options;
     }
 
-}
+    void runCorrectness(
+        std::size_t batch_size,
+        std::size_t channels,
+        std::size_t input_height,
+        std::size_t input_width,
+        std::size_t output_height,
+        std::size_t output_width,
+        std::size_t kernel_height,
+        std::size_t kernel_width,
+        std::size_t stride_height,
+        std::size_t stride_width,
+        std::size_t padding_height,
+        std::size_t padding_width)
+    {
+        const auto input = kl::test::makeRandomTensor(
+            kl::Shape{
+                batch_size,
+                channels,
+                input_height,
+                input_width},
+            kl::DType::Float32,
+            kl::Device::cuda(),
+            -1.0,
+            1.0,
+            11);
 
-TEST(MaxPool2dForwardCudaFloat32, MatchesCuDNN)
-{
-    const auto input = kl::test::makeRandomTensor(
-        kl::Shape{
-            options::batch_size,
-            options::channels,
-            options::input_height,
-            options::input_width},
-        kl::DType::Float32,
-        kl::Device::cuda(),
-        -1.0,
-        1.0,
-        11);
+        kl::Tensor actual(
+            kl::Shape{
+                batch_size,
+                channels,
+                output_height,
+                output_width},
+            kl::DType::Float32,
+            kl::Device::cuda());
 
-    kl::Tensor actual(
-        kl::Shape{
-            options::batch_size,
-            options::channels,
-            options::output_height,
-            options::output_width},
-        kl::DType::Float32,
-        kl::Device::cuda());
+        const kl::Pooling2dOptions pooling_options =
+            make_pooling_options(
+                kernel_height,
+                kernel_width,
+                stride_height,
+                stride_width,
+                padding_height,
+                padding_width);
 
-    const kl::Pooling2dOptions pooling_options =
-        make_pooling_options();
-
-    kl::maxpool2d_cuda_float32(
-        input,
-        actual,
-        pooling_options);
-
-    const auto expected =
-        kl::test::cudnnMaxPool2dForwardFloat32(
+        kl::maxpool2d_cuda_float32(
             input,
-            options::kernel_height,
-            options::kernel_width,
-            options::padding_height,
-            options::padding_width,
-            options::stride_height,
-            options::stride_width);
+            actual,
+            pooling_options);
 
-    EXPECT_TRUE(kl::test::tensorCompare(
-        expected,
-        actual,
-        options::absolute_tolerance,
-        options::relative_tolerance));
+        const auto expected =
+            kl::test::cudnnMaxPool2dForwardFloat32(
+                input,
+                kernel_height,
+                kernel_width,
+                padding_height,
+                padding_width,
+                stride_height,
+                stride_width);
+
+        EXPECT_TRUE(kl::test::tensorCompare(
+            expected,
+            actual,
+            options::absolute_tolerance,
+            options::relative_tolerance));
+    }
+
+    void runBenchmark(
+        const char *benchmark_name,
+        std::size_t batch_size,
+        std::size_t channels,
+        std::size_t input_height,
+        std::size_t input_width,
+        std::size_t output_height,
+        std::size_t output_width,
+        std::size_t kernel_height,
+        std::size_t kernel_width,
+        std::size_t stride_height,
+        std::size_t stride_width,
+        std::size_t padding_height,
+        std::size_t padding_width)
+    {
+        const auto input = kl::test::makeRandomTensor(
+            kl::Shape{
+                batch_size,
+                channels,
+                input_height,
+                input_width},
+            kl::DType::Float32,
+            kl::Device::cuda(),
+            -1.0,
+            1.0,
+            11);
+
+        kl::Tensor kerneloom_output(
+            kl::Shape{
+                batch_size,
+                channels,
+                output_height,
+                output_width},
+            kl::DType::Float32,
+            kl::Device::cuda());
+
+        kl::Tensor cudnn_output(
+            kl::Shape{
+                batch_size,
+                channels,
+                output_height,
+                output_width},
+            kl::DType::Float32,
+            kl::Device::cuda());
+
+        const kl::Pooling2dOptions pooling_options =
+            make_pooling_options(
+                kernel_height,
+                kernel_width,
+                stride_height,
+                stride_width,
+                padding_height,
+                padding_width);
+
+        kl::test::CudnnHandle cudnn_handle;
+
+        kl::test::CudnnTensorDescriptor input_descriptor(
+            input);
+
+        kl::test::CudnnTensorDescriptor cudnn_output_descriptor(
+            cudnn_output);
+
+        kl::test::CudnnPoolingDescriptor cudnn_pooling_descriptor(
+            kernel_height,
+            kernel_width,
+            padding_height,
+            padding_width,
+            stride_height,
+            stride_width);
+
+        const double kerneloom_ms = kl::test::benchmarkGpu(
+            kl::Device::cuda(),
+            options::warmup_iterations,
+            options::measured_iterations,
+            options::print_each_iteration,
+            "Kerneloom CUDA",
+            [&]()
+            {
+                kl::maxpool2d_cuda_float32(
+                    input,
+                    kerneloom_output,
+                    pooling_options);
+            });
+
+        const double cudnn_ms = kl::test::benchmarkGpu(
+            kl::Device::cuda(),
+            options::warmup_iterations,
+            options::measured_iterations,
+            options::print_each_iteration,
+            "cuDNN",
+            [&]()
+            {
+                kl::test::cudnnMaxPool2dForwardFloat32(
+                    cudnn_handle,
+                    input_descriptor,
+                    cudnn_output_descriptor,
+                    cudnn_pooling_descriptor,
+                    input,
+                    cudnn_output);
+            });
+
+        EXPECT_TRUE(kl::test::tensorCompare(
+            cudnn_output,
+            kerneloom_output,
+            options::absolute_tolerance,
+            options::relative_tolerance));
+
+        kl::test::printBenchmarkComparison(
+            benchmark_name,
+            "Kerneloom CUDA",
+            kerneloom_ms,
+            "cuDNN",
+            cudnn_ms);
+    }
+
 }
 
-TEST(MaxPool2dForwardCudaFloat32, BenchmarkAgainstCuDNN)
+TEST(MaxPool2dForwardCudaFloat32, MatchesCuDNN_RegularLarge)
 {
-    const auto input = kl::test::makeRandomTensor(
-        kl::Shape{
-            options::batch_size,
-            options::channels,
-            options::input_height,
-            options::input_width},
-        kl::DType::Float32,
-        kl::Device::cuda(),
-        -1.0,
-        1.0,
-        11);
+    runCorrectness(
+        options::regular_large::batch_size,
+        options::regular_large::channels,
+        options::regular_large::input_height,
+        options::regular_large::input_width,
+        options::regular_large::output_height,
+        options::regular_large::output_width,
+        options::regular_large::kernel_height,
+        options::regular_large::kernel_width,
+        options::regular_large::stride_height,
+        options::regular_large::stride_width,
+        options::regular_large::padding_height,
+        options::regular_large::padding_width);
+}
 
-    kl::Tensor kerneloom_output(
-        kl::Shape{
-            options::batch_size,
-            options::channels,
-            options::output_height,
-            options::output_width},
-        kl::DType::Float32,
-        kl::Device::cuda());
+TEST(MaxPool2dForwardCudaFloat32, MatchesCuDNN_DemandingOddShape)
+{
+    runCorrectness(
+        options::demanding_odd_shape::batch_size,
+        options::demanding_odd_shape::channels,
+        options::demanding_odd_shape::input_height,
+        options::demanding_odd_shape::input_width,
+        options::demanding_odd_shape::output_height,
+        options::demanding_odd_shape::output_width,
+        options::demanding_odd_shape::kernel_height,
+        options::demanding_odd_shape::kernel_width,
+        options::demanding_odd_shape::stride_height,
+        options::demanding_odd_shape::stride_width,
+        options::demanding_odd_shape::padding_height,
+        options::demanding_odd_shape::padding_width);
+}
 
-    kl::Tensor cudnn_output(
-        kl::Shape{
-            options::batch_size,
-            options::channels,
-            options::output_height,
-            options::output_width},
-        kl::DType::Float32,
-        kl::Device::cuda());
+TEST(MaxPool2dForwardCudaFloat32, MatchesCuDNN_PaddedStrideOne)
+{
+    runCorrectness(
+        options::padded_stride_one::batch_size,
+        options::padded_stride_one::channels,
+        options::padded_stride_one::input_height,
+        options::padded_stride_one::input_width,
+        options::padded_stride_one::output_height,
+        options::padded_stride_one::output_width,
+        options::padded_stride_one::kernel_height,
+        options::padded_stride_one::kernel_width,
+        options::padded_stride_one::stride_height,
+        options::padded_stride_one::stride_width,
+        options::padded_stride_one::padding_height,
+        options::padded_stride_one::padding_width);
+}
 
-    const kl::Pooling2dOptions pooling_options =
-        make_pooling_options();
+TEST(MaxPool2dForwardCudaFloat32, MatchesCuDNN_PaddedStrideTwo)
+{
+    runCorrectness(
+        options::padded_stride_two::batch_size,
+        options::padded_stride_two::channels,
+        options::padded_stride_two::input_height,
+        options::padded_stride_two::input_width,
+        options::padded_stride_two::output_height,
+        options::padded_stride_two::output_width,
+        options::padded_stride_two::kernel_height,
+        options::padded_stride_two::kernel_width,
+        options::padded_stride_two::stride_height,
+        options::padded_stride_two::stride_width,
+        options::padded_stride_two::padding_height,
+        options::padded_stride_two::padding_width);
+}
 
-    kl::test::CudnnHandle cudnn_handle;
+TEST(MaxPool2dForwardCudaFloat32, BenchmarkAgainstCuDNN_RegularLarge)
+{
+    runBenchmark(
+        "MaxPool2d Forward CUDA Float32 Regular Large",
+        options::regular_large::batch_size,
+        options::regular_large::channels,
+        options::regular_large::input_height,
+        options::regular_large::input_width,
+        options::regular_large::output_height,
+        options::regular_large::output_width,
+        options::regular_large::kernel_height,
+        options::regular_large::kernel_width,
+        options::regular_large::stride_height,
+        options::regular_large::stride_width,
+        options::regular_large::padding_height,
+        options::regular_large::padding_width);
+}
 
-    kl::test::CudnnTensorDescriptor input_descriptor(
-        input);
+TEST(MaxPool2dForwardCudaFloat32, BenchmarkAgainstCuDNN_DemandingOddShape)
+{
+    runBenchmark(
+        "MaxPool2d Forward CUDA Float32 Demanding Odd Shape",
+        options::demanding_odd_shape::batch_size,
+        options::demanding_odd_shape::channels,
+        options::demanding_odd_shape::input_height,
+        options::demanding_odd_shape::input_width,
+        options::demanding_odd_shape::output_height,
+        options::demanding_odd_shape::output_width,
+        options::demanding_odd_shape::kernel_height,
+        options::demanding_odd_shape::kernel_width,
+        options::demanding_odd_shape::stride_height,
+        options::demanding_odd_shape::stride_width,
+        options::demanding_odd_shape::padding_height,
+        options::demanding_odd_shape::padding_width);
+}
 
-    kl::test::CudnnTensorDescriptor cudnn_output_descriptor(
-        cudnn_output);
+TEST(MaxPool2dForwardCudaFloat32, BenchmarkAgainstCuDNN_PaddedStrideOne)
+{
+    runBenchmark(
+        "MaxPool2d Forward CUDA Float32 Padded Stride One",
+        options::padded_stride_one::batch_size,
+        options::padded_stride_one::channels,
+        options::padded_stride_one::input_height,
+        options::padded_stride_one::input_width,
+        options::padded_stride_one::output_height,
+        options::padded_stride_one::output_width,
+        options::padded_stride_one::kernel_height,
+        options::padded_stride_one::kernel_width,
+        options::padded_stride_one::stride_height,
+        options::padded_stride_one::stride_width,
+        options::padded_stride_one::padding_height,
+        options::padded_stride_one::padding_width);
+}
 
-    kl::test::CudnnPoolingDescriptor cudnn_pooling_descriptor(
-        options::kernel_height,
-        options::kernel_width,
-        options::padding_height,
-        options::padding_width,
-        options::stride_height,
-        options::stride_width);
-
-    const double kerneloom_ms = kl::test::benchmarkGpu(
-        kl::Device::cuda(),
-        options::warmup_iterations,
-        options::measured_iterations,
-        options::print_each_iteration,
-        "Kerneloom CUDA",
-        [&]()
-        {
-            kl::maxpool2d_cuda_float32(
-                input,
-                kerneloom_output,
-                pooling_options);
-        });
-
-    const double cudnn_ms = kl::test::benchmarkGpu(
-        kl::Device::cuda(),
-        options::warmup_iterations,
-        options::measured_iterations,
-        options::print_each_iteration,
-        "cuDNN",
-        [&]()
-        {
-            kl::test::cudnnMaxPool2dForwardFloat32(
-                cudnn_handle,
-                input_descriptor,
-                cudnn_output_descriptor,
-                cudnn_pooling_descriptor,
-                input,
-                cudnn_output);
-        });
-
-    EXPECT_TRUE(kl::test::tensorCompare(
-        cudnn_output,
-        kerneloom_output,
-        options::absolute_tolerance,
-        options::relative_tolerance));
-
-    kl::test::printBenchmarkComparison(
-        "MaxPool2d Forward CUDA Float32",
-        "Kerneloom CUDA",
-        kerneloom_ms,
-        "cuDNN",
-        cudnn_ms);
+TEST(MaxPool2dForwardCudaFloat32, BenchmarkAgainstCuDNN_PaddedStrideTwo)
+{
+    runBenchmark(
+        "MaxPool2d Forward CUDA Float32 Padded Stride Two",
+        options::padded_stride_two::batch_size,
+        options::padded_stride_two::channels,
+        options::padded_stride_two::input_height,
+        options::padded_stride_two::input_width,
+        options::padded_stride_two::output_height,
+        options::padded_stride_two::output_width,
+        options::padded_stride_two::kernel_height,
+        options::padded_stride_two::kernel_width,
+        options::padded_stride_two::stride_height,
+        options::padded_stride_two::stride_width,
+        options::padded_stride_two::padding_height,
+        options::padded_stride_two::padding_width);
 }
 
 #endif // KL_ENABLE_CUDA
