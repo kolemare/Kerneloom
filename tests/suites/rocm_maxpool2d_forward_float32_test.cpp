e2329659b0d556a061f1a2387655949a8c966489
Marko Kostic <kolemareee@gmail.com>
2026-07-20 03:18:08 +0200

    Expand ROCm MaxPool2d Benchmark Suite

--- tests/suites/rocm_maxpool2d_forward_float32_test.cpp
+++ tests/suites/rocm_maxpool2d_forward_float32_test.cpp
@@ -19,181 +19,366 @@
 
 #include <kernels/rocm/pooling/maxpool2d_rocm_float32.hiph>
 
+#include <cstddef>
+
 namespace
 {
 
     namespace options =
         kl::test::options::maxpool2d_forward_float32;
 
-    kl::Pooling2dOptions make_pooling_options()
+    kl::Pooling2dOptions make_pooling_options(
+        std::size_t kernel_height,
+        std::size_t kernel_width,
+        std::size_t stride_height,
+        std::size_t stride_width,
+        std::size_t padding_height,
+        std::size_t padding_width)
     {
         kl::Pooling2dOptions pooling_options;
 
         pooling_options.kernel_h =
-            options::kernel_height;
+            kernel_height;
 
         pooling_options.kernel_w =
-            options::kernel_width;
+            kernel_width;
 
         pooling_options.stride_h =
-            options::stride_height;
+            stride_height;
 
         pooling_options.stride_w =
-            options::stride_width;
+            stride_width;
 
         pooling_options.padding_h =
-            options::padding_height;
+            padding_height;
 
         pooling_options.padding_w =
-            options::padding_width;
+            padding_width;
 
         return pooling_options;
     }
 
-}
+    void runCorrectness(
+        std::size_t batch_size,
+        std::size_t channels,
+        std::size_t input_height,
+        std::size_t input_width,
+        std::size_t output_height,
+        std::size_t output_width,
+        std::size_t kernel_height,
+        std::size_t kernel_width,
+        std::size_t stride_height,
+        std::size_t stride_width,
+        std::size_t padding_height,
+        std::size_t padding_width)
+    {
+        const auto input = kl::test::makeRandomTensor(
+            kl::Shape{
+                batch_size,
+                channels,
+                input_height,
+                input_width},
+            kl::DType::Float32,
+            kl::Device::rocm(),
+            -1.0,
+            1.0,
+            11);
 
-TEST(MaxPool2dForwardRocmFloat32, MatchesMIOpen)
-{
-    const auto input = kl::test::makeRandomTensor(
-        kl::Shape{
-            options::batch_size,
-            options::channels,
-            options::input_height,
-            options::input_width},
-        kl::DType::Float32,
-        kl::Device::rocm(),
-        -1.0,
-        1.0,
-        11);
+        kl::Tensor actual(
+            kl::Shape{
+                batch_size,
+                channels,
+                output_height,
+                output_width},
+            kl::DType::Float32,
+            kl::Device::rocm());
 
-    kl::Tensor actual(
-        kl::Shape{
-            options::batch_size,
-            options::channels,
-            options::output_height,
-            options::output_width},
-        kl::DType::Float32,
-        kl::Device::rocm());
+        const kl::Pooling2dOptions pooling_options =
+            make_pooling_options(
+                kernel_height,
+                kernel_width,
+                stride_height,
+                stride_width,
+                padding_height,
+                padding_width);
 
-    const kl::Pooling2dOptions pooling_options =
-        make_pooling_options();
-
-    kl::maxpool2d_rocm_float32(
-        input,
-        actual,
-        pooling_options);
-
-    const auto expected =
-        kl::test::miopenMaxPool2dForwardFloat32(
+        kl::maxpool2d_rocm_float32(
             input,
-            options::kernel_height,
-            options::kernel_width,
-            options::padding_height,
-            options::padding_width,
-            options::stride_height,
-            options::stride_width);
+            actual,
+            pooling_options);
 
-    EXPECT_TRUE(kl::test::tensorCompare(
-        expected,
-        actual,
-        options::absolute_tolerance,
-        options::relative_tolerance));
+        const auto expected =
+            kl::test::miopenMaxPool2dForwardFloat32(
+                input,
+                kernel_height,
+                kernel_width,
+                padding_height,
+                padding_width,
+                stride_height,
+                stride_width);
+
+        EXPECT_TRUE(kl::test::tensorCompare(
+            expected,
+            actual,
+            options::absolute_tolerance,
+            options::relative_tolerance));
+    }
+
+    void runBenchmark(
+        const char *benchmark_name,
+        std::size_t batch_size,
+        std::size_t channels,
+        std::size_t input_height,
+        std::size_t input_width,
+        std::size_t output_height,
+        std::size_t output_width,
+        std::size_t kernel_height,
+        std::size_t kernel_width,
+        std::size_t stride_height,
+        std::size_t stride_width,
+        std::size_t padding_height,
+        std::size_t padding_width)
+    {
+        const auto input = kl::test::makeRandomTensor(
+            kl::Shape{
+                batch_size,
+                channels,
+                input_height,
+                input_width},
+            kl::DType::Float32,
+            kl::Device::rocm(),
+            -1.0,
+            1.0,
+            11);
+
+        kl::Tensor kerneloom_output(
+            kl::Shape{
+                batch_size,
+                channels,
+                output_height,
+                output_width},
+            kl::DType::Float32,
+            kl::Device::rocm());
+
+        kl::Tensor miopen_output(
+            kl::Shape{
+                batch_size,
+                channels,
+                output_height,
+                output_width},
+            kl::DType::Float32,
+            kl::Device::rocm());
+
+        const kl::Pooling2dOptions pooling_options =
+            make_pooling_options(
+                kernel_height,
+                kernel_width,
+                stride_height,
+                stride_width,
+                padding_height,
+                padding_width);
+
+        kl::test::MiopenHandle miopen_handle;
+
+        kl::test::MiopenTensorDescriptor input_descriptor(
+            input);
+
+        kl::test::MiopenTensorDescriptor miopen_output_descriptor(
+            miopen_output);
+
+        kl::test::MiopenPoolingDescriptor miopen_pooling_descriptor(
+            kernel_height,
+            kernel_width,
+            padding_height,
+            padding_width,
+            stride_height,
+            stride_width);
+
+        const double kerneloom_ms = kl::test::benchmarkGpu(
+            kl::Device::rocm(),
+            options::warmup_iterations,
+            options::measured_iterations,
+            options::print_each_iteration,
+            "Kerneloom ROCm",
+            [&]()
+            {
+                kl::maxpool2d_rocm_float32(
+                    input,
+                    kerneloom_output,
+                    pooling_options);
+            });
+
+        const double miopen_ms = kl::test::benchmarkGpu(
+            kl::Device::rocm(),
+            options::warmup_iterations,
+            options::measured_iterations,
+            options::print_each_iteration,
+            "MIOpen",
+            [&]()
+            {
+                kl::test::miopenMaxPool2dForwardFloat32(
+                    miopen_handle,
+                    input_descriptor,
+                    miopen_output_descriptor,
+                    miopen_pooling_descriptor,
+                    input,
+                    miopen_output);
+            });
+
+        EXPECT_TRUE(kl::test::tensorCompare(
+            miopen_output,
+            kerneloom_output,
+            options::absolute_tolerance,
+            options::relative_tolerance));
+
+        kl::test::printBenchmarkComparison(
+            benchmark_name,
+            "Kerneloom ROCm",
+            kerneloom_ms,
+            "MIOpen",
+            miopen_ms);
+    }
+
 }
 
-TEST(MaxPool2dForwardRocmFloat32, BenchmarkAgainstMIOpen)
+TEST(MaxPool2dForwardRocmFloat32, MatchesMIOpen_RegularLarge)
 {
-    const auto input = kl::test::makeRandomTensor(
-        kl::Shape{
-            options::batch_size,
-            options::channels,
-            options::input_height,
-            options::input_width},
-        kl::DType::Float32,
-        kl::Device::rocm(),
-        -1.0,
-        1.0,
-        11);
+    runCorrectness(
+        options::regular_large::batch_size,
+        options::regular_large::channels,
+        options::regular_large::input_height,
+        options::regular_large::input_width,
+        options::regular_large::output_height,
+        options::regular_large::output_width,
+        options::regular_large::kernel_height,
+        options::regular_large::kernel_width,
+        options::regular_large::stride_height,
+        options::regular_large::stride_width,
+        options::regular_large::padding_height,
+        options::regular_large::padding_width);
+}
 
-    kl::Tensor kerneloom_output(
-        kl::Shape{
-            options::batch_size,
-            options::channels,
-            options::output_height,
-            options::output_width},
-        kl::DType::Float32,
-        kl::Device::rocm());
+TEST(MaxPool2dForwardRocmFloat32, MatchesMIOpen_DemandingOddShape)
+{
+    runCorrectness(
+        options::demanding_odd_shape::batch_size,
+        options::demanding_odd_shape::channels,
+        options::demanding_odd_shape::input_height,
+        options::demanding_odd_shape::input_width,
+        options::demanding_odd_shape::output_height,
+        options::demanding_odd_shape::output_width,
+        options::demanding_odd_shape::kernel_height,
+        options::demanding_odd_shape::kernel_width,
+        options::demanding_odd_shape::stride_height,
+        options::demanding_odd_shape::stride_width,
+        options::demanding_odd_shape::padding_height,
+        options::demanding_odd_shape::padding_width);
+}
 
-    kl::Tensor miopen_output(
-        kl::Shape{
-            options::batch_size,
-            options::channels,
-            options::output_height,
-            options::output_width},
-        kl::DType::Float32,
-        kl::Device::rocm());
+TEST(MaxPool2dForwardRocmFloat32, MatchesMIOpen_PaddedStrideOne)
+{
+    runCorrectness(
+        options::padded_stride_one::batch_size,
+        options::padded_stride_one::channels,
+        options::padded_stride_one::input_height,
+        options::padded_stride_one::input_width,
+        options::padded_stride_one::output_height,
+        options::padded_stride_one::output_width,
+        options::padded_stride_one::kernel_height,
+        options::padded_stride_one::kernel_width,
+        options::padded_stride_one::stride_height,
+        options::padded_stride_one::stride_width,
+        options::padded_stride_one::padding_height,
+        options::padded_stride_one::padding_width);
+}
 
-    const kl::Pooling2dOptions pooling_options =
-        make_pooling_options();
+TEST(MaxPool2dForwardRocmFloat32, MatchesMIOpen_PaddedStrideTwo)
+{
+    runCorrectness(
+        options::padded_stride_two::batch_size,
+        options::padded_stride_two::channels,
+        options::padded_stride_two::input_height,
+        options::padded_stride_two::input_width,
+        options::padded_stride_two::output_height,
+        options::padded_stride_two::output_width,
+        options::padded_stride_two::kernel_height,
+        options::padded_stride_two::kernel_width,
+        options::padded_stride_two::stride_height,
+        options::padded_stride_two::stride_width,
+        options::padded_stride_two::padding_height,
+        options::padded_stride_two::padding_width);
+}
 
-    kl::test::MiopenHandle miopen_handle;
+TEST(MaxPool2dForwardRocmFloat32, BenchmarkAgainstMIOpen_RegularLarge)
+{
+    runBenchmark(
+        "MaxPool2d Forward ROCm Float32 Regular Large",
+        options::regular_large::batch_size,
+        options::regular_large::channels,
+        options::regular_large::input_height,
+        options::regular_large::input_width,
+        options::regular_large::output_height,
+        options::regular_large::output_width,
+        options::regular_large::kernel_height,
+        options::regular_large::kernel_width,
+        options::regular_large::stride_height,
+        options::regular_large::stride_width,
+        options::regular_large::padding_height,
+        options::regular_large::padding_width);
+}
 
-    kl::test::MiopenTensorDescriptor input_descriptor(
-        input);
+TEST(MaxPool2dForwardRocmFloat32, BenchmarkAgainstMIOpen_DemandingOddShape)
+{
+    runBenchmark(
+        "MaxPool2d Forward ROCm Float32 Demanding Odd Shape",
+        options::demanding_odd_shape::batch_size,
+        options::demanding_odd_shape::channels,
+        options::demanding_odd_shape::input_height,
+        options::demanding_odd_shape::input_width,
+        options::demanding_odd_shape::output_height,
+        options::demanding_odd_shape::output_width,
+        options::demanding_odd_shape::kernel_height,
+        options::demanding_odd_shape::kernel_width,
+        options::demanding_odd_shape::stride_height,
+        options::demanding_odd_shape::stride_width,
+        options::demanding_odd_shape::padding_height,
+        options::demanding_odd_shape::padding_width);
+}
 
-    kl::test::MiopenTensorDescriptor miopen_output_descriptor(
-        miopen_output);
+TEST(MaxPool2dForwardRocmFloat32, BenchmarkAgainstMIOpen_PaddedStrideOne)
+{
+    runBenchmark(
+        "MaxPool2d Forward ROCm Float32 Padded Stride One",
+        options::padded_stride_one::batch_size,
+        options::padded_stride_one::channels,
+        options::padded_stride_one::input_height,
+        options::padded_stride_one::input_width,
+        options::padded_stride_one::output_height,
+        options::padded_stride_one::output_width,
+        options::padded_stride_one::kernel_height,
+        options::padded_stride_one::kernel_width,
+        options::padded_stride_one::stride_height,
+        options::padded_stride_one::stride_width,
+        options::padded_stride_one::padding_height,
+        options::padded_stride_one::padding_width);
+}
 
-    kl::test::MiopenPoolingDescriptor miopen_pooling_descriptor(
-        options::kernel_height,
-        options::kernel_width,
-        options::padding_height,
-        options::padding_width,
-        options::stride_height,
-        options::stride_width);
-
-    const double kerneloom_ms = kl::test::benchmarkGpu(
-        kl::Device::rocm(),
-        options::warmup_iterations,
-        options::measured_iterations,
-        options::print_each_iteration,
-        "Kerneloom ROCm",
-        [&]()
-        {
-            kl::maxpool2d_rocm_float32(
-                input,
-                kerneloom_output,
-                pooling_options);
-        });
-
-    const double miopen_ms = kl::test::benchmarkGpu(
-        kl::Device::rocm(),
-        options::warmup_iterations,
-        options::measured_iterations,
-        options::print_each_iteration,
-        "MIOpen",
-        [&]()
-        {
-            kl::test::miopenMaxPool2dForwardFloat32(
-                miopen_handle,
-                input_descriptor,
-                miopen_output_descriptor,
-                miopen_pooling_descriptor,
-                input,
-                miopen_output);
-        });
-
-    EXPECT_TRUE(kl::test::tensorCompare(
-        miopen_output,
-        kerneloom_output,
-        options::absolute_tolerance,
-        options::relative_tolerance));
-
-    kl::test::printBenchmarkComparison(
-        "MaxPool2d Forward ROCm Float32",
-        "Kerneloom ROCm",
-        kerneloom_ms,
-        "MIOpen",
-        miopen_ms);
+TEST(MaxPool2dForwardRocmFloat32, BenchmarkAgainstMIOpen_PaddedStrideTwo)
+{
+    runBenchmark(
+        "MaxPool2d Forward ROCm Float32 Padded Stride Two",
+        options::padded_stride_two::batch_size,
+        options::padded_stride_two::channels,
+        options::padded_stride_two::input_height,
+        options::padded_stride_two::input_width,
+        options::padded_stride_two::output_height,
+        options::padded_stride_two::output_width,
+        options::padded_stride_two::kernel_height,
+        options::padded_stride_two::kernel_width,
+        options::padded_stride_two::stride_height,
+        options::padded_stride_two::stride_width,
+        options::padded_stride_two::padding_height,
+        options::padded_stride_two::padding_width);
 }
 
 #endif // KL_ENABLE_ROCM
